64905104301d2cdbb5f2b2b0941dc305cfbc6408
KyofuNoKamen <oleksiykaras2016@gmail.com>
2021-10-07 20:03:38 +0300

    Map and View

--- Game/Map.h
+++ Game/Map.h
@@ -14,9 +14,13 @@ class Map
 public:
 	Map(sf::RenderWindow*);
 	~Map();
+	void insert_into_tile_set(std::string arr_key[], sf::IntRect arr_coordinates[], int max);
+	sf::IntRect set_texture(std::string tile_name);
 private:
 	Tiles* grid;
 	sf::RenderWindow* main_window;
+	std::map <std::string, sf::IntRect> tile_set;
+	std::map <std::string, sf::IntRect> ::iterator iterator_tile_set;
 	
 	void draw_map(std::string map[10][10]);
 
