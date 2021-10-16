4439d1ede5e56ada2fa0f4e6339bd8faf82d072c
Ophiuchus86 <korobkodmitrij02@gmail.com>
2021-10-16 14:48:36 +0300

    Load map from .tmx file

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "Window.h"
+#include "Level.h"
+
 
 int main()
 {
-    Window main_window(1000, 1000, "Main window");
+    // Level - это типо карта
+    Level level;
+    level.LoadFromFile("resources/map.tmx");
+    Window main_window(1000, 1000, level, "Main window");
 
     main_window.start();
 
--- Game/Window.cpp
+++ Game/Window.cpp
@@ -6,6 +6,12 @@ Window::Window(int resolution_x, int resolution_y, std::string name)
 	create_window(resolution_x, resolution_y, name);
 }
 
+Window::Window(int resolution_x, int resolution_y, Level& level, std::string name)
+    : Window(resolution_x, resolution_y, name)
+{
+    setLevel(level);
+}
+
 void Window::create_window(int resolution_x, int resolution_y, std::string name)
 {
     Window::main_window.create(sf::VideoMode(resolution_x, resolution_y), name);
@@ -18,14 +24,19 @@ sf::RenderWindow &Window::get_window()
     return main_window;
 }
 
+void Window::setLevel(Level& level) {
+    this->level = &level;
+}
+
 void Window::start() 
 {
-
     Hero hero(&get_window());
+
     sf::View view;
     view.setCenter(sf::Vector2f(500, 500));
     main_window.setView(view);
-    Map map(&get_window());
+
+    //Map map(&get_window());
 
     while (main_window.isOpen())
     {
@@ -46,9 +57,9 @@ void Window::start()
         }
         
         main_window.clear();
-        map.draw_map();
+        //hero.move();
+        if (level) level->Draw(main_window);
         hero.heroSpriteFunction();
-        hero.move();
         main_window.display();
     }
 }
