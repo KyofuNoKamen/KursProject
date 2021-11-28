379a660bef7180187fdda1c5b316a85c0a811f34
KyofuNoKamen <oleksiykaras2016@gmail.com>
2021-11-28 21:39:40 +0200

    added network tests

--- Game/Engine/Networking.cpp
+++ Game/Engine/Networking.cpp
@@ -1,58 +1,68 @@
 #include "../Headers/Networking.h"
 #include <thread>
+#include <iostream>
+#include <stdlib.h>
 
+sf::Thread *thr;
+sf::Mutex mutex;
 
 void Networking::StartServer() {
     printf("before startS");
-    std::thread thr(&Networking::executionThread, this);
-    
+    std::cout << std::endl << sf::IpAddress::getPublicAddress() << std::endl;
+    thr = new sf::Thread(&Networking::executionThread, this);
+
+    thr->launch();
+    Initialize();
 }
 
 void Networking::executionThread() {
-    Initialize();
-    //while(isAlive) loop();
+    sf::Clock clock_;
+    while (1) {
+        mutex.lock();
+        loop();
+        sf::sleep(sf::milliseconds(10));
+        mutex.unlock();
+    }
+        
     printf("loop");
-    loop();
     printf("shutdown");
     shutdown();
 }
 
 void Networking::Initialize() {
-    isAlive = true;
-    printf("i1");
     // bind the listener to a port
-    if (listener.listen(port) != sf::Socket::Done) {
-        printf("ERROR!Listening to port %d failed.", port);
-    }
+    client.bind(client_port);
+    server.bind(server_port);    
 
-    sf::TcpSocket client;
-    if (listener.accept(client) != sf::Socket::Done) {
-        printf("ERROR!Failed to accept client.");
-    }
-    sf::Packet packet;
-    packet << 1;
-    printf("i3");
-    client.send(packet);
+    send_message("first message!");
+    send_message("second message!");
 }
 
 void Networking::loop() {
 
-    sf::TcpSocket socket;
-    printf("l1");
-    sf::Socket::Status status = socket.connect(sf::IpAddress::getLocalAddress(), port);
-    if (status != sf::Socket::Done)
+
+
+    //server.setBlocking(0);
+
+    char buffer[1024];
+    std::size_t received = 0;
+    sf::IpAddress sender;
+    unsigned short port;
+    if (server.receive(buffer, sizeof(buffer), received, sender, port) == 0)
     {
-        printf("Client connection failed!\n");
+        std::cout << std::endl << sender.getPublicAddress().toString() << " said: " << buffer << std::endl;
     }
-    printf("l2");
-    sf::Packet pck;
-    //socket.receive(pck);
-    int a=1;
-    //pck >> a;
-    printf("a=%d", a);
+    //std::cout << port;
 }
 
 void Networking::shutdown() {
 
+}
+
+void Networking::send_message(std::string message)
+{
+    std::string snd_message = message;
+    //std::cout << message << std::endl;
+    client.send(snd_message.c_str(), snd_message.size() + 1, "192.168.1.101", server_port);
 }
 
--- Game/Headers/Networking.h
+++ Game/Headers/Networking.h
@@ -8,12 +8,17 @@ public:
 	void executionThread();
 private:
 	sf::SocketSelector selector;
-	sf::TcpListener listener;
+	sf::UdpSocket client;
+	sf::UdpSocket server;
 	std::vector<sf::TcpSocket> clients;
-	const unsigned short port = 7777;
+	unsigned short client_port = 55001;
+	unsigned short server_port = 55002;
 	bool isAlive;
 	
 	void Initialize();
 	void loop();
 	void shutdown();
+
+	void send_message(std::string message);
+
 };
